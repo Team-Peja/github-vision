cee3840eb39f3c928af2ee4f4a7758b5c651b7d6
justin.ko43 <justin.ko43@gmail.com>
2018-01-29 20:52:06 -0800

    react component architecture finished

---
+++ client/components/Graph/Graph.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Graph extends Component {
+  render() {
+    return (
+      <div class="graph">
+        GRAPH HERE!!!!
+      </div>
+    )
+  }
+}
+
+export default Graph;
--- client/components/container.jsx
+++ client/components/container.jsx
@@ -8,11 +8,21 @@ import Main from './Main/Main.jsx';
 import Footer from './Footer/Footer.jsx';
 
 class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: {}};
+  }
+
+  //do fetch for authorization here
+  componentWillMount() {
+
+  }
+
   render() {
     return (
     <div id="container">
       <Nav />
-      <Main />
+      <Main user={this.state.user}/>
       <Footer />
     </div>
     )
