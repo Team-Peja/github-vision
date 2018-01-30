a5507177dfc73f06221e58b20315b20a9b4c422a
justin.ko43 <justin.ko43@gmail.com>
2018-01-30 12:04:38 -0800

    finished styling nav bar

--- client/components/Nav/Nav.jsx
+++ client/components/Nav/Nav.jsx
@@ -5,13 +5,12 @@ import logo from '../../assets/images/glasses.svg'
 class Nav extends Component {
   render() {
     return (
-      <div id="Nav">
+      <div id="Nav" className="bg-gr0 padding-left-xl padding-right-xl box-shadow-light">
         <div id="logo">
           <img src={logo} />
-          <h4 className="align-vertical">Git Visual</h4>
+          <h4 className="raleway text-gr3 margin-left-m">Git Visual</h4>
         </div>
-        <div></div>
-        <nav id="navigation">
+        <nav id="navigation" className="margin-right-xl">
         <ul className="fw-600 nav-container">
           <li className="hide-nav-link"><a href="">How it Works</a></li>
           <li className="hide-nav-link"><a href="">Log in with Github!</a></li>
