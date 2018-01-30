1891a615aec911c0396b6477f9fe97c54616dc12
justin.ko43 <justin.ko43@gmail.com>
2018-01-29 17:36:17 -0800

    favicon loads

--- client/components/Main/Main.jsx
+++ client/components/Main/Main.jsx
@@ -4,8 +4,11 @@ import { render } from 'react-dom';
 class Main extends Component {
   render() {
     return (
-    <div id="main">
-      stuff
+    <div id="main" className="section padding">
+      <div id="instructions">
+        <div className="h2 fw-600 margin-top margin-bottom-m raleway">Get Started</div>
+        {/* {this.renderInstructions()} */}
+      </div>
     </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const favicon = require('serve-favicon');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(favicon(path.join(__dirname, '../build/assets/images', 'glasses.ico')));
 app.use('/build', express.static(path.join(__dirname, '/../', 'build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on port 3000!');
+app.listen(process.env.PORT || 1234, () => {
+  console.log('listening on port 1234!');
 });
