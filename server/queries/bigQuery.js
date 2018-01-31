3ade37cac7090c1f5368686bfeddfe2c37bfac86
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-31 13:57:51 -0800

    move graphql queries to new folder

---
+++ server/queries/bigQuery.js
@@ -0,0 +1,63 @@
+module.exports = function(id) {
+  return (`
+    {
+      viewer {
+        id
+        login
+        name
+        avatarUrl
+        email
+        createdAt
+        updatedAt
+        followers {
+          totalCount
+        }
+        following {
+          totalCount
+        }
+        repositories(last: 100) {
+          totalCount
+          nodes {
+            id
+            name
+            forkCount
+            languages(first: 10) {
+              totalCount
+              totalSize
+              edges {
+                size
+                node {
+                  name
+                }
+              }
+            }
+            ... on Repository {
+              ref(qualifiedName: "master") {
+                target {
+                  ... on Commit {
+                    history(first: 50, author:{id:"${id}"}) {
+                      totalCount
+                      edges {
+                        node {
+                          id
+                          additions
+                          deletions
+                          author {
+                            user {
+                              login
+                            }
+                            date
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`
+  )
+}
